6b474318874ee19f8b44f0f5a7ace1a98941cc5d
aylax <zhoubye@foxmail.com>
2021-08-19 14:29:36 +0800

    new :: suites -> define workstation and import it into hosts

--- flake.nix
+++ flake.nix
@@ -11,18 +11,30 @@
         url = "github:hercules-ci/hercules-ci-agent";
         inputs = { nix-darwin.follows = "darwin"; nixos-20_09.follows = "nixos"; nixos-unstable.follows = "latest"; };
       };
+
       darwin.url = "github:LnL7/nix-darwin";
       darwin.inputs.nixpkgs.follows = "latest";
+
       home.url = "github:nix-community/home-manager";
       home.inputs.nixpkgs.follows = "nixos";
+
       naersk.url = "github:nmattia/naersk";
       naersk.inputs.nixpkgs.follows = "latest";
+
       agenix.url = "github:ryantm/agenix";
       agenix.inputs.nixpkgs.follows = "latest";
+
       nixos-hardware.url = "github:nixos/nixos-hardware";
 
       pkgs.url = "path:./pkgs";
       pkgs.inputs.nixpkgs.follows = "nixos";
+
+      emacs.url = "github:nix-community/emacs-overlay";
+
+      neovim.url = "github:nix-community/neovim-nightly-overlay";
+
+      nixpkgs.follows = "nixos";
+
     };
 
   outputs =
@@ -35,6 +47,8 @@
     , nixos-hardware
     , nur
     , agenix
+    , emacs
+    , neovim
     , ...
     } @ inputs:
     digga.lib.mkFlake {
@@ -50,6 +64,8 @@
             pkgs.overlay # for `srcs`
             nur.overlay
             agenix.overlay
+	    emacs.overlay
+	    neovim.overlay
           ];
         };
         latest = { };
@@ -91,6 +107,22 @@
           };
           suites = with profiles; rec {
             base = [ core users.aylax users.root ];
+	    workstation = [
+
+	      # hosts
+	      core
+
+	      # users
+	      users.root
+	      users.aylax
+
+	      # profiles
+	      network
+	      openssh
+	      develop
+	      printing
+	      graphical
+	    ];
           };
         };
       };
