6dac8af91dec88f06d17281dc3f9a161e5bcc676
aylax <zhoubye@foxmail.com>
2021-08-19 14:10:13 +0800

    new :: profiles.openssh -> add openssh module

---
+++ profiles/openssh/default.nix
@@ -0,0 +1,12 @@
+{ ... }: 
+
+{
+  services.openssh = {
+    enable = true;
+    challengeResponseAuthentication = false;
+    passwordAuthentication = false;
+    forwardX11 = true;
+    permitRootLogin = "no";
+    startWhenNeeded = true;
+  };
+}
